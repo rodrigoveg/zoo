5699327fba487e4a7964ec0d56a76169a4e5d100
Rodrigo Vega <jr_veg@hotmail.com>
2016-10-24 23:23:36 -0500

    manual validation added length of name

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,3 +1,11 @@
 class Animal < ActiveRecord::Base
   belongs_to :animal_type
+
+  validate :name_valid
+
+  def name_valid
+    if self.name.length < 5
+      errors.add(:name, 'Name must be 5 character minimum')
+    end
+  end
 end
